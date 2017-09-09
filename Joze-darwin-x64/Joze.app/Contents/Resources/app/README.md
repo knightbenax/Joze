b4ce595661f9f1495ec85f220a0f778f09e42eb2
Bezaleel Ashefor <knightbenax@gmail.com>
2017-09-09 23:18:30 +0100

    Mango

---
+++ Joze-darwin-x64/Joze.app/Contents/Resources/app/README.md
@@ -0,0 +1,6 @@
+# Joze
+Instagram Client for Mac. Built with Electron
+
+[![Knightbenax Approved](http://ephodng.com/knightbenax-badge.svg)](https://twitter.com/knightbenax)
+
+![Joze Screenshot](/joze.png?raw=true)
--- Joze-darwin-x64/Joze.app/Contents/Resources/app/main.js
+++ Joze-darwin-x64/Joze.app/Contents/Resources/app/main.js
@@ -22,14 +22,19 @@ function createWindow() {
        height: 820,
        x: x_position,
        y: y_position,
+       enableSwipe: true,
        useLastState: true,
        fakeUserAgent: true,
-       agent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+       //agent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
+       agent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2_1 like Mac OS X) AppleWebKit/602.4.6 (KHTML, like Gecko) Version/10.0 Mobile/14D27 Safari/602.1',
        titleBarStyle: 'hidden-inset',
        maximizable: false,
        fullscreenable: false,
-       resizable: false
+       resizable: false,
+       show:false,
+       title: 'Joze'
     }) 
+    
    win.loadURL('http://instagram.com'
     /*url.format ({ 
       pathname: path.join(__dirname, 'index.html'), 
@@ -45,6 +50,26 @@ function createWindow() {
         }
     })
    })
+
+   win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  win.on('app-command', (e, cmd) => {
+    // Navigate the window back when the user hits their mouse back button
+    if (cmd === 'browser-backward' && win.webContents.canGoBack()) {
+      win.webContents.goBack()
+    }
+  })
+
+  win.on('swipe', (e, direction) => {
+    // Navigate the window back when the user hits their mouse back button
+      //if (direction == "left"){
+        win.webContents.goBack()
+      //}
+  })
 }  
+
+//app.dock.setBadge('Joze - Instagram Desktop Client')
 
 app.on('ready', createWindow) 
